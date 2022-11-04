fb83076d3becff95f23fbb78ba9cb65782aad9d8
Mohomed Rashad <mohomedrashad.2000@gmail.com>
2022-11-04 11:35:47 +0530

    fix: Overridden the perform_create method instead of the create method in the availability viewset

--- apps/GPService/views.py
+++ apps/GPService/views.py
@@ -8,23 +8,5 @@ class AvailabilityViewSet(viewsets.ModelViewSet):
     queryset = Availability.objects.all()
     serializer_class = AvailabilitySerializer
 
-    def list(self, request, *args, **kwargs):
-        queryset = Availability.objects.all()
-        serializer = AvailabilitySerializer(queryset, many=True)
-        return Response(serializer.data)
-
-    def retrieve(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = AvailabilitySerializer(instance=instance)
-        return Response(serializer.data)
-
-    def create(self, request):
-        if request.user.is_authenticated and request.user.role == "DOCTOR":
-            serializer = AvailabilitySerializer(data=request.data)
-            if serializer.is_valid(raise_exception=True):
-                serializer.save(doctor=request.user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-            #return Response(status=status.HTTP_401_UNAUTHORIZED)
-        else:
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
-
+    def perform_create(self, serializer):
+        serializer.save(doctor=self.request.user)
