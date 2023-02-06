824c0b89ee6b257584663038babae0f18b0561f2
Mohomed Rashad <mohomedrashad.2000@gmail.com>
2023-02-06 12:37:41 +0530

    refactor: changed the database port to 5432

--- project/settings.py
+++ project/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'USER': os.environ.get('DATABASE_USER', 'postgres'),
         'PASSWORD': os.environ.get('DATABASE_PWD', 'password'),
         'HOST': os.environ.get('DATABASE_HOST', 'localhost'),
-        'PORT': os.environ.get("DATABASE_PORT", '2222'),
+        'PORT': os.environ.get("DATABASE_PORT", '5432'),
         'TEST': {
             'NAME': 'boilerplate_test',
         },
