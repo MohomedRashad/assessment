25b3b3b8d5b4ecfa099422917f2ad36b42864974
MohomedRashad <mohomedrashad.2000@gmail.com>
2023-06-23 12:48:41 +0530

    feat: created the invoice model

--- apps/GPService/models.py
+++ apps/GPService/models.py
@@ -222,6 +222,15 @@ class Order(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     total_amount = models.PositiveIntegerField(blank=True, null=True)
 
+class Invoice(models.Model):
+    order = models.OneToOneField(Order, on_delete=models.CASCADE, related_name='invoice')
+    invoice_number = models.CharField(max_length=20, unique=True)
+    date = models.DateField(default=timezone.now)
+    invoice = models.TextField()
+
+    def __str__(self):
+        return self.invoice_number
+
 class Country(models.Model):
     name = models.CharField(max_length=50, unique=True)
 
